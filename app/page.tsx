5c244c65b4fa9ee4d9c9fd1fe9457c42c74362cb
A12M9x91 <131251678+A12M9x91@users.noreply.github.com>
2023-05-21 10:42:26 +0330

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default async function Home() {
   return (
     <main className="max-w-4xl mx-auto mt-4">
       <div className="text-center my-5 flex flex-col gap-4">
-        <h1 className="text-2xl font-bold"> Jelly Todos</h1>
+        <h1 className="text-2xl font-bold">List</h1>
         <AddTask></AddTask>
       </div>
       <TodoList tasks={tasks}/>
